8c832bbd1a2ec629582bedb6e19ff106cda52fdc
chrisdortch <chrisdortch@gmail.com>
2026-06-04 20:59:36 -0500

    Add Next config

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**.suno.com' },
+      { protocol: 'https', hostname: '**.suno.ai' },
+      { protocol: 'https', hostname: 'cdn1.suno.ai' },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
+    ]
+  }
+};
+export default nextConfig;
